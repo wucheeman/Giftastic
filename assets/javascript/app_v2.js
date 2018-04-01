b50afb765d9b4664a640c5c8e33681ef3261ff22
Mark Hainline <wucheeman@gmail.com>
2018-04-01 17:08:50 -0400

    final code clean up, with linting, prior to 3rd round test

--- assets/javascript/app_v2.js
+++ assets/javascript/app_v2.js
@@ -10,28 +10,26 @@ let activities;
 
 const addButton = (newActivity) => {
   // adds a new button based on user's input
-  // normalizes input t whitespace
-  // does not add empty strings or repeats
   // TODO (future): should reject non-words comprised only of letters
-  console.log("in addButton()");
+  // console.log("in addButton()");
   newActivity = cleanInput(newActivity);
   if (checkForDupsOrEmptyString(newActivity) && checkForBadChars(newActivity)) {
-    console.log('ok to make a button');
+    // console.log('ok to make a button');
     activities.push(newActivity);
-    console.log(activities);
+    // console.log(activities);
     // clear text input
     $('#button-input').val('');
     // initializeDisplay();
     var imgElements = makeButtons();
-    render(imgElements, "#button-land", 'empty');
+    render(imgElements, '#button-land', 'empty');
     $('#warning-land').addClass('text-success text-center');
     $('#warning-land').html('<strong>new button has been added</strong>');
     clearWarningLand();
   }
   else {
-    console.log('Input must be only letters')
+    // console.log('Input must be only letters')
     $('#warning-land').addClass('text-danger text-center');
-    $('#warning-land').html("<strong>Not a new activity!</strong>");
+    $('#warning-land').html('<strong>Not a new activity!</strong>');
     $('#button-input').val('');
     clearWarningLand();
   }
@@ -51,17 +49,17 @@ const checkForBadChars = (newActivity) => {
 const checkForDupsOrEmptyString = (newActivity) => {
   // checks for duplicates or empty strng and returns false if one is found
   let userInputIsGood = true;
-  if (activities.includes(newActivity) || newActivity === "") {
+  if (activities.includes(newActivity) || newActivity === '') {
     userInputIsGood = false;
   }
   return userInputIsGood;
 }
 
 const checkResponseForGifs = (response) => {
-  console.log('in checkResponseForGifs()');
+  // console.log('in checkResponseForGifs()');
   let responseHasGifs = true;
   if (response.data.length === 0) {
-    console.log('no data in response');
+    // console.log('no data in response');
     responseHasGifs = false;
   }
   return responseHasGifs;
@@ -69,42 +67,40 @@ const checkResponseForGifs = (response) => {
 
 const cleanInput = (newActivity) => {
   // cleans and normalizes input for new buttons
-  console.log("in cleanInput()");
+  // console.log("in cleanInput()");
   return newActivity.trim().toLowerCase();
 }
 
 const clearWarningLand = () => {
   setTimeout(() => {
-    render([], "#warning-land", 'empty');
+    render([], '#warning-land', 'empty');
   }, 3000);
 }
 
 const clickHandler = (e) => {
-  console.log("in clickHandler");
-  // TODO: refactor to use switch (future?)
-  console.log(e.target);
-  console.log(e);
+  // console.log("in clickHandler");
+  // TODO: refactor to use switch (future)
+  // console.log(e.target);
   if (e.target.className === 'btn btn-info mr-2 mb-2 activity') {
-    console.log('found a button!');
+    // console.log('found a button!');
     // call api and display the gifs
-    console.log(e.target.id);
+    // console.log(e.target.id);
     search(e.target.id);
   }
   else if (e.target.className === 'gif'){
-    // TODO: reduce verbosity during cleanup
+    // TODO: (future) reduce verbosity 
     const element = $(e.target);
-    console.log('in clickHandler: element = ' + element);
+    // console.log('in clickHandler: element = ' + element);
     toggleGif(element);
   }
   else if (e.target.className === 'add-activity') {
     e.defaultPrevented;
-    var newActivity = $("#button-input").val();
-    console.log('newActivity is: ' + newActivity);
+    var newActivity = $('#button-input').val();
+    // console.log('newActivity is: ' + newActivity);
     addButton(newActivity);
   }
   else {
-    console.log('not supposed to get here in clickHandler');
-
+    // console.log('not supposed to get here in clickHandler');
   }
 }
 
@@ -114,22 +110,20 @@ const initializeGlobals = () => {
 
 const initializeDisplay = () => {
   // lays out buttons using the activities array
-  // TODO: in v2, adds form for additional buttons
   var imgElements = makeButtons();
-  render(imgElements, "#button-land");
+  render(imgElements, '#button-land');
 }
 
 const main = () => {
   // initializes globals and display; enables app to begin taking user clicks
-  console.log('in main()');
+  // console.log('in main()');
   initializeGlobals();
   initializeDisplay();
-  $(document).on("click", clickHandler);
+  $(document).on('click', clickHandler);
 }
 
 // btn-info mr-2 mb-2 
 const makeButtons = () => {
-  var imgElements = 0;
   var buttons = [];
   for (var i = 0; i < activities.length; i++ ) {
     var button = `<button class="btn btn-info mr-2 mb-2 activity" id=${activities[i]}>${activities[i]}</button>`;
@@ -149,36 +143,36 @@ const makeRatingElement = (rating) => {
 
 const processResponse = (response) => {
   // takes API response object and makes URLs for display on page
-  console.log("in processResponse()");
+  // console.log("in processResponse()");
   if (checkResponseForGifs(response)) {
-    console.log('response has gifs');
+    // console.log('response has gifs');
     let imgElements = [];
     let ratingsElements = [];
     for (var i = 0; i < 10; i++) {
-      console.log("making URLs");
+      // console.log("making URLs");
       const stillURL = response.data[i].images.fixed_height_still.url;
-      console.log('stillURL is ' + stillURL);
+      // console.log('stillURL is ' + stillURL);
       const animatedURL = response.data[i].images.fixed_height.url;
-      console.log("animatedURL is " + animatedURL);
+      // console.log("animatedURL is " + animatedURL);
       const newImageHtml = makeImageElement(stillURL, animatedURL);
-      console.log("newImageHtml is " + newImageHtml);
+      // console.log("newImageHtml is " + newImageHtml);
       imgElements.push(newImageHtml);
       const rating = response.data[i].rating;
-      console.log('rating is: ' + rating);
+      // console.log('rating is: ' + rating);
       const newRatingElement = makeRatingElement(rating);
-      console.log("new rating element is " + newRatingElement);
+      // console.log("new rating element is " + newRatingElement);
       ratingsElements.push(newRatingElement);
     }
     // add ratingsElements to display; requires work in image_display.js
     imgElements = makeImageDisplay(imgElements, ratingsElements);
-    console.log("before calling render, imgElements is" + imgElements);
+    // console.log("before calling render, imgElements is" + imgElements);
     render(imgElements, "#gify-land", 'empty');
   }
   else {
-    console.log('No gifs were returned from that search');
+    // console.log('No gifs were returned from that search');
     $('#warning-land').addClass('text-danger text-center');
-    $('#warning-land').html("<strong>No gifs were returned from that search!</strong>")
-    render([], "#gify-land", 'empty');
+    $('#warning-land').html('<strong>No gifs were returned from that search!</strong>');
+    render([], '#gify-land', 'empty');
     clearWarningLand();
   }
 }
@@ -186,65 +180,59 @@ const processResponse = (response) => {
 const render = (imgElements, location, action) => {
   // updates screen at location with contents of imgElements after taking
   // the action, if one is specified;
-  console.log("in render");
-  // TODO: change this so it can do something besides empty?
-  // TODO: use different argument type?
+  // console.log("in render");
+  // currently, only supports emptying the location
   if (action) {
-    console.log("render has an action to take");
+    // console.log("render has an action to take");
     $(location).empty();
   }
-  for (i = 0; i < imgElements.length; i++) {
+  for (var i = 0; i < imgElements.length; i++) {
     $(location).append(imgElements[i]);
   }
 }
 
 function search(search) {
-  console.log("in search");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=2iJQK64mjf8ANz5A14Gtwn09rOBldGhG&q=" + search;
+  // console.log("in search");
+  var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=2iJQK64mjf8ANz5A14Gtwn09rOBldGhG&q=' + search;
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-      processResponse(response);
-    });
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+    // console.log(response);
+    processResponse(response);
+  });
 }
 
 const toggleGif = (element) => {
   // toggles gif between static image and animated when gif is clicked
-  console.log("in toggleGif");
-  const STATE_STILL = "still";
-  const STATE_ANIMATE = "animate";
-        
-  // $(".gif").on("click", function() {
-  //   console.log("in ToggleGif's on-click");
-    // const element = $(this);
-    // console.log(element);
-    const state = element.attr('data-state');
-    console.log("state is now: " + state);
-    console.log('the current image source is: ' + element.attr('src'));
+  // console.log("in toggleGif");
+  const STATE_STILL = 'still';
+  const STATE_ANIMATE = 'animate';
 
-    if (state === STATE_STILL) {
-      console.log("toggling to animation");
-      element.attr('src', element.attr('data-animate'));
-      console.log('the image source has changed to be: ' + element.attr('src'));
-      element.attr('data-state', STATE_ANIMATE);
-      console.log("should be animated now");
-    } else {
-      console.log("toggling to still image");
-      element.attr('src', element.attr('data-still'));
-      console.log('the image source has changed to be: ' + element.attr('src'));
-      element.attr('data-state', STATE_STILL);
-    }
-  // });
+  const state = element.attr('data-state');
+  // console.log("state is now: " + state);
+  // console.log('the current image source is: ' + element.attr('src'));
+
+  if (state === STATE_STILL) {
+    // console.log("toggling to animation");
+    element.attr('src', element.attr('data-animate'));
+    // console.log('the image source has changed to be: ' + element.attr('src'));
+    element.attr('data-state', STATE_ANIMATE);
+    // console.log("should be animated now");
+  } else {
+    // console.log("toggling to still image");
+    element.attr('src', element.attr('data-still'));
+    // console.log('the image source has changed to be: ' + element.attr('src'));
+    element.attr('data-state', STATE_STILL);
+  }
 }
 
 // APPLICATION
 //===============================================================
 
 $(document).ready(function() {
-  console.log('Started');
+  // console.log('Started');
   main();
 });
 
--- assets/javascript/image_display.js
+++ assets/javascript/image_display.js
@@ -1,108 +1,105 @@
 // Builds image display html so it can be sent to the
 // render function in app.js
 
-    const makeImageDisplay = (imageArray, ratingsArray) => {
-      // takes array of html-wrapped images and converts to html ready for display
-      console.log("in makeImageDisplay");
-      const rowPairs = [];
-      for (i = 0; i < imageArray.length; i+=3) {
-        gifs = imageArray.slice(i, i + 3);
-        ratings =ratingsArray.slice(i, i + 3);
-        console.log("output of loop is gifs: " + gifs + ' and ratings: ' + ratings);
-        rowPairs.push(makeRowPair(ratings, gifs));
-      }
-      return rowPairs;
-    }
+const makeImageDisplay = (imageArray, ratingsArray) => {
+  // takes array of html-wrapped images and converts to html ready for display
+  // console.log("in makeImageDisplay");
+  const rowPairs = [];
+  for (i = 0; i < imageArray.length; i+=3) {
+    gifs = imageArray.slice(i, i + 3);
+    ratings =ratingsArray.slice(i, i + 3);
+    // console.log("output of loop is gifs: " + gifs + ' and ratings: ' + ratings);
+    rowPairs.push(makeRowPair(ratings, gifs));
+  }
+  return rowPairs;
+}
 
-    const makeRowPair = (ratings, gifs) => {
-      // pairs together a rating row and an image row
-      // TODO (future) brittle; refactor when time permits
-      console.log("in makeRowPair");
-      if (gifs.length === 3) {
-        console.log("making regular length row pair");
-        const ratingRow = makeRatingRow(ratings);
-        const imageRow = makeImageRow(gifs);
-        return ratingRow + imageRow;
-      }
-      else if (gifs.length === 1) {
-        console.log("making short row pair");
-        const ratingRow = makeShortRatingRow(ratings);
-        const imageRow = makeShortImageRow(gifs);
-        return ratingRow + imageRow;
-      }
-      else {
-        console.log("makeRowPair has unsupported gif array length");
-      }
-    }
+const makeRowPair = (ratings, gifs) => {
+  // pairs together a rating row and an image row
+  // console.log("in makeRowPair");
+  if (gifs.length === 3) {
+    // console.log("making regular length row pair");
+    const ratingRow = makeRatingRow(ratings);
+    const imageRow = makeImageRow(gifs);
+    return ratingRow + imageRow;
+  }
+  else if (gifs.length === 1) {
+    // console.log("making short row pair");
+    const ratingRow = makeShortRatingRow(ratings);
+    const imageRow = makeShortImageRow(gifs);
+    return ratingRow + imageRow;
+  }
+  else {
+    // console.log("makeRowPair has unsupported gif array length");
+  }
+}
 
-    const makeImageRow = (gifs) => {
-      // takes an array of gifs and converts to a bootstrap row of images
-      const newRow = `
-          <div class="row">
-            <div class="col-sm-4 mb-3 img-fluid imgRow">
-              ${gifs[0]}
-            </div>
-            <div class="col-sm-4 mb-3 img-fluid imgRow">
-              ${gifs[1]}
-            </div>
-            <div class="col-sm-4 mb-3 img-fluid imgRow">
-              ${gifs[2]}
-            </div>
-          </div>
-        `
-      return newRow;
-    }
-
-    const makeShortImageRow = (gifs) => {
-      // takes an array of gifs and converts to a bootstrap row of images
-      const newRow = `
-          <div class="row">
-            <div class="col-sm-4 mb-3">
-              ${gifs[0]}
-            </div>
-            <div class="col-sm-4 mb-3">
-            </div>
-            <div class="col-sm-4 mb-3">
-            </div>
-          </div>
-        `
-      return newRow;
-    }
-
-    const makeRatingRow = (ratings) => {
-      // displays ratings
-      // TODO: fill in with actual ratings
-      console.log("in makeRatingRow()");
-      const ratingRow = `
+const makeImageRow = (gifs) => {
+  // takes an array of gifs and converts to a bootstrap row of images
+  const newRow = `
       <div class="row">
-        <div class="col-sm-4 mt-3">
-          ${ratings[0]}
+        <div class="col-sm-4 mb-3 img-fluid imgRow">
+          ${gifs[0]}
         </div>
-        <div class="col-sm-4 mt-3">
-          ${ratings[1]}
+        <div class="col-sm-4 mb-3 img-fluid imgRow">
+          ${gifs[1]}
         </div>
-        <div class="col-sm-4 mt-3">
-          ${ratings[2]}
+        <div class="col-sm-4 mb-3 img-fluid imgRow">
+          ${gifs[2]}
         </div>
       </div>
-      `
-      console.log('rating row is ' + ratingRow);
-      return ratingRow;
-    }
+    `;
+  return newRow;
+}
 
-    const makeShortRatingRow = (ratings) => {
-      // displays ratings
-      // TODO: fill in with actual ratings
-      const ratingRow = `
+const makeShortImageRow = (gifs) => {
+  // takes an array of gifs and converts to a bootstrap row of images
+  const newRow = `
       <div class="row">
-        <div class="col-sm-4 mt-3">
-          ${ratings[0]}
+        <div class="col-sm-4 mb-3">
+          ${gifs[0]}
         </div>
-        <div class="col-sm-4 mt-3">
+        <div class="col-sm-4 mb-3">
         </div>
-        <div class="col-sm-4 mt-3">
+        <div class="col-sm-4 mb-3">
         </div>
       </div>
-      `
-      return ratingRow;
-    }
+    `;
+  return newRow;
+}
+
+const makeRatingRow = (ratings) => {
+  // displays ratings
+  // console.log("in makeRatingRow()");
+  const ratingRow = `
+  <div class="row">
+    <div class="col-sm-4 mt-3">
+      ${ratings[0]}
+    </div>
+    <div class="col-sm-4 mt-3">
+      ${ratings[1]}
+    </div>
+    <div class="col-sm-4 mt-3">
+      ${ratings[2]}
+    </div>
+  </div>
+  `;
+  // console.log('rating row is ' + ratingRow);
+  return ratingRow;
+}
+
+const makeShortRatingRow = (ratings) => {
+  // displays ratings
+  const ratingRow = `
+  <div class="row">
+    <div class="col-sm-4 mt-3">
+      ${ratings[0]}
+    </div>
+    <div class="col-sm-4 mt-3">
+    </div>
+    <div class="col-sm-4 mt-3">
+    </div>
+  </div>
+  `;
+  return ratingRow;
+}
